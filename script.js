be9281c496198bac3389fd42787638c5d3ce1574
Pranjal Thakkar <imcae22060@gmail.com>
2024-06-05 08:20:35 +0000

    Functions in JS

--- script.js
+++ script.js
@@ -1,62 +1,57 @@
-// primitive and reference
-// data types primitive and reference
-// primitive => string, number, boolean 
-// reference => [] {}
-
-// let a =true;
-// let b = a;
-// b = 7;
-
-// // let a = [1,2,3]
-// // let b = a;
-// // b.pop()
-
-// console.log(a, b)
-
-// let  a = 5;
-// let b = a;
-// b =10;
-
-// console.log(a,b)
-
-
-
-// let a = [1,2,3]
-// let b = a;
-// b.push(4)
-
-// console.log(a,b)
-
-
-
 // conditionals
-// anything that is true or false or converted to true or false weca n write that inside condition
-// if(""){
-//     console.log('party 🥳')
+// age range
+// if the age is less than 12 => can not participate
+// if the age is between 12 to 16 => under 16 comp.
+// if the age is between 17 to 20 => can participate in senior level
+// else age is greater than 20 can not participate
+
+let age = 19;
+
+// if(age<12){
+//     console.log('can not participate')
+// }
+// // && only executes when both the conditions are correct
+// else if(age >= 12 && age <= 16){
+//     console.log('under 16')
+// }
+// else if(age >=17 && age <=20){
+//     console.log('senior level')
 // }
 // else{
-//     console.log('movie 🍿')
+//     console.log('can not participate')
 // }
 
-// thruthy and falsy
-// falsy => 0 -0 "" '' NaN undefined null
-// truthy => execute as true condition
-let x = 24;
-console.log(typeof x)
+// ternary operator
+// condition ?(true) {true} :(else) {false}
+// age>= 18 ? console.log('eligible to vote') : console.log('not eligible to vote');
 
 
 
-let name = prompt("Enter the name");
-let age = prompt("Enter the age");
-if(typeof age == "number"){
-if(age > 18){
-    console.log(name + ' is eligibe to vote!!')
-}
-else{
-    console.log(name + ' is not eligibe to vote!!')
+// functions is a reusable code to perform same task when we want
 
-}
-}
-else{
-    console.log('Enter the correct age')
+// function grinder(items){
+//     console.log('grinding...', items)
+// }
+
+// grinder('tomatoes');
+// grinder('onions')
+
+
+// function add(a, b){
+
+//     return a+b;
+// }
+
+// let sum1 = add(2,3)
+// let sum2 = add(12,45)
+
+// console.log(sum1, sum2)
+
+
+function joined(){
+    let title = document.getElementById('title');
+    title.innerText = 'Thanks for joining the bootcamp live!'
+    let btn = document.getElementById('btn');
+    // btn.style.display = 'none'
+    btn.style.backgroundColor = 'blueviolet'
 }
