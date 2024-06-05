84b814a858212bd0080ca408688bdc1bfb3bee40
Pranjal Thakkar <imcae22060@gmail.com>
2024-06-05 14:52:11 +0000

    Array Mapping and Filters

--- script.js
+++ script.js
@@ -1,108 +1,86 @@
-function joined() {
-    let title = document.getElementById('title');
-    title.innerText = 'Thanks for joining  the bootcamp!!'
-}
+// console.log(a)
+// arrow function
 
-function convert() {
-    let m = document.getElementById('meterInput');
-    let cm = m.value * 100;
-    let finalResult = m.value + " M is " + cm + " CM"
-    let result = document.getElementById('result');
-    result.innerText = finalResult;
-    // make input field blank
-    m.value = ''
-}
+// function functionName(){
+//     logic
+// }
 
-function split() {
-    let amount = document.getElementById('amount');
-    let persons = document.getElementById('persons');
+// functionName = (x)=>{
+//     // console.log('arrow fucntion is called', x)
+
+// }
+// functionName(5)
 
 
-    let bill = (amount.value / persons.value).toFixed(0);
-    document.getElementById('result').innerText = bill
-}
+// sqr = (n)=> n*n;
 
-let users = []
-
-function notifyUserAdded(){
-    let alert = document.getElementById('alert-success');
-    alert.classList.remove('alert');
-    alert.classList.add('alert-success')
-
-    setTimeout(() => {
-            alert.classList.remove('alert-success');
-            alert.classList.add('alert');
-    }, 2000);
-
-}
+// function cube(n){
+//     return n*n;
+// }
 
 
-function notifyUserExist(){
-    let alert = document.getElementById('alert-danger')
-    alert.classList.remove('alert');
-    alert.classList.add('alert-danger')
-
-    setTimeout(() => {
-        alert.classList.remove('alert-danger')
-        alert.classList.add('alert');
-    }, 2000);
-
-    // 2000 is milliseconds
-}
+// console.log(sqr(4))
 
 
-function addUser(userInput){
-    let checkUser = users.filter((user)=>{
-        return user.email == userInput.email;
-    })
-    if(checkUser.length == 0){
-        users.push(userInput)
-        notifyUserAdded();
-    }
-    else{
-        notifyUserExist();
-    }
+// callbacks
 
-}
+
+// console.log('task 1')
+// task will execute in 2 sec
+// setTimeout(()=>{
+//     console.log('task 2')
+// },2000)
+// console.log('task 3')
+
+
+// callback is also a function
+// function taskDone(){
+//     console.log('task is done!!')
+// }
 
 
 
-function displayUsers() {
-    let usersContainer = document.getElementById('users');
-    usersContainer.innerHTML = " "
-    users.map((a) => {
-        let userDiv = document.createElement('div');
-        userDiv.classList.add('user')
-        let nameElement = document.createElement('p');
-        nameElement.innerText = a.name;
-        let emailElement = document.createElement('p');
-        emailElement.innerText = a.email;
 
-        usersContainer.appendChild(userDiv);
-        userDiv.appendChild(nameElement)
-        userDiv.appendChild(emailElement)
-    })
+let a = [1,2,3,4,5,6,7,8]
+
+// a.forEach((n)=>{
+//     console.log(n)
+// })
+
+
+// a.forEach(function(n){
+//     console.log(n)
+// })
 
 
 
-}
+// day 2
+
+// add = (a,b)=> a+b;
+// console.log(add(2,3))
+
+// foreach can not return;
+// let arr = [1,2,3,4]
+
+// let result = arr.forEach((a)=>{
+//     return a*a
+// })
+
+// console.log(result)
+// callback
 
 
 
-function add() {
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
+// array mapping and filters
+let arr = [1,2,3,4]
 
-    let user = {
-        name: name.value,
-        email: email.value,
-    }
+// let result = arr.map((number)=>{
+//     return number*number;
+// })
 
-    addUser(user)
-    displayUsers();
 
-    console.log(users)
-}
+// let result = arr.filter((n)=>{
+//     return n%2 == 0
+// })
 
-let demo = document.getElementById('demo');
-demo.innerHTML = '<h1>this is demo of innerText</h1>'
+// console.log(result)
