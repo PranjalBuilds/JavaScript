299c81d0473e54d6daecc5b411fe32b0d1c531f2
Pranjal Thakkar <imcae22060@gmail.com>
2024-06-06 16:36:34 +0000

    Promises in JS

--- app.js
+++ app.js
@@ -1,88 +1,41 @@
-const users = [
-  {
-    firstName: "Aditi",
-    lastName: "Sharma",
-    age: 25,
-    gender: "female",
-  },
-  {
-    firstName: "Arnav",
-    lastName: "Singh",
-    age: 27,
-    gender: "male",
-  },
-  {
-    firstName: "Chhavi",
-    lastName: "Mehta",
-    age: 23,
-    gender: "female",
-  },
-  {
-    firstName: "Dhruv",
-    lastName: "Agarwal",
-    age: 28,
-    gender: "male",
-  },
-  {
-    firstName: "Eesha",
-    lastName: "Gupta",
-    age: 22,
-    gender: "female",
-  },
-  {
-    firstName: "Gaurav",
-    lastName: "Dwivedi",
-    age: 26,
-    gender: "male",
-  },
-  {
-    firstName: "Harsh",
-    lastName: "Verma",
-    age: 29,
-    gender: "male",
-  },
-  {
-    firstName: "Ishita",
-    lastName: "Banerjee",
-    age: 24,
-    gender: "female",
-  },
-  {
-    firstName: "Jatin",
-    lastName: "Kumar",
-    age: 20,
-    gender: "male",
-  },
-];
+// console.log('task 1')
+// setTimeout(()=>{
+//     console.log('download started')
+// },2000)
+// console.log('download completed')
 
-// foreach
-// let firstNames = users.forEach((user)=>{
-//     console.log(user.firstName)
+// let startDownload = new Promise ((resolve, reject)=>{
+//     console.log('download started')
+//     setTimeout(()=>{
+//         reject();
+//     },3000)
 // })
 
-// mapping
-// let firstNames = users.map((user)=>{
-//     return user.firstName + " " + user.lastName;
+// startDownload.then(()=> {
+//     console.log('download completed')
+// }, () =>{
+//     console.log('download failed')
 // })
 
-// console.log(firstNames)
 
+// fetch = used to fetch data from url => promise
 
-// let females = users.filter((user)=>{
-//     return user.gender == 'female'
-// }).map((user)=>{
-//     return user.firstName;
+// let randomQuoteIndex = Math.floor(Math.random() * 15)
+// console.log(randomQuoteIndex)
+// fetch('https://type.fit/api/quotes').then((resp) => {
+//     return resp.json()
+// }).then((quotes) => {
+//     console.log(quotes[randomQuoteIndex].text)
 // })
 
-// console.log(females)
+fetchQuote=()=>{
+    let randomQuoteIndex = Math.floor(Math.random() * 15)
+    fetch('https://type.fit/api/quotes').then((resp) => {
+        return resp.json()
+    }).then((quotes) => {
+        document.getElementById('quote').innerText = quotes[randomQuoteIndex].text;
+        document.getElementById('author').innerText = quotes[randomQuoteIndex].author;
+    })
+}
 
-
-let males = users.filter((user)=>{
-
-      return user.gender == 'male' && user.age> 25;
-
-}).map((user)=>{
-  return user.firstName
-})
-
-console.log(males)
+fetchQuote()
