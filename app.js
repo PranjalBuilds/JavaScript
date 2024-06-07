b2fc20abdab56e7b82581741e178b4ada904a63b
Pranjal Thakkar <imcae22060@gmail.com>
2024-06-07 16:55:00 +0000

    Movie Search Project

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-// console.log('task 1')
-// setTimeout(()=>{
-//     console.log('download started')
-// },2000)
-// console.log('download completed')
+let api = 'https://www.omdbapi.com/?apikey=61e576a4&t='
 
-// let startDownload = new Promise ((resolve, reject)=>{
-//     console.log('download started')
-//     setTimeout(()=>{
-//         reject();
-//     },3000)
-// })
-
-// startDownload.then(()=> {
-//     console.log('download completed')
-// }, () =>{
-//     console.log('download failed')
-// })
+let title = document.getElementById('title');
+let date = document.getElementById('date');
+let director = document.getElementById('director');
+let actors = document.getElementById('actors');
+let genre = document.getElementById('genre');
+let desc = document.getElementById('desc');
+let collection = document.getElementById('collection');
+let awards = document.getElementById('awards');
+let writer = document.getElementById('writer');
+let ratings = document.getElementById('ratings');
+let img = document.getElementById('img');
 
 
-// fetch = used to fetch data from url => promise
 
-// let randomQuoteIndex = Math.floor(Math.random() * 15)
-// console.log(randomQuoteIndex)
-// fetch('https://type.fit/api/quotes').then((resp) => {
-//     return resp.json()
-// }).then((quotes) => {
-//     console.log(quotes[randomQuoteIndex].text)
-// })
+let container = document.getElementById('container');
+let loader = document.getElementById('animation-loader')
+container.classList.add('d-none')
+loader.classList.add('d-none')
 
-fetchQuote=()=>{
-    let randomQuoteIndex = Math.floor(Math.random() * 15)
-    fetch('https://type.fit/api/quotes').then((resp) => {
-        return resp.json()
-    }).then((quotes) => {
-        document.getElementById('quote').innerText = quotes[randomQuoteIndex].text;
-        document.getElementById('author').innerText = quotes[randomQuoteIndex].author;
+
+
+function searchMovie() {
+    loader.classList.remove('d-none')
+    let movieName = document.getElementById('movieName');
+    let query = api + movieName.value;
+    fetch(query).then((data) => {
+        return data.json()
+    }).then((data) => {
+        loader.classList.add('d-none')
+        container.classList.remove('d-none');
+        title.innerText = data.Title;
+        date.innerText = data.Released;
+        director.innerText = data.Director;
+        actors.innerText = data.Actors;
+        genre.innerText = data.Genre;
+        desc.innerText = data.Plot;
+        collection.innerText = data.BoxOffice;
+        awards.innerText = data.Awards;
+        ratings.innerText = data.imdbRating;
+        writer.innerText = data.Writer;
+        img.src = data.Poster;
     })
 }
-
-fetchQuote()
